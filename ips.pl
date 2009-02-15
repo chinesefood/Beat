eaaa0a4644f155cb9d522b6e2cd1bcb7e2d07786
chinesefood <not@ava.il.ab.le>
2009-02-14 22:35:31 -0600

    Added descriptive error messages.

--- ips.pl
+++ ips.pl
@@ -26,21 +26,23 @@ unless (@ARGV == 2) {
 
 my $patch;
 DETECT_PATCH: for (my $i = 0; $i < @ARGV; $i++) {
-    open(PATCH, $ARGV[$i]) or die "Can't open $ARGV[$i] for reading.\n";
+    open(PATCH, $ARGV[$i]) or die "open() failed opening $ARGV[$i] for reading.\n";
 
     read(PATCH, my $header, 5);
     $patch = splice(@ARGV, $i, 1) if $header eq 'PATCH';
-last DETECT_PATCH if $patch;
+
+    last DETECT_PATCH if $patch;
 
     close(PATCH);
 }
-die("No IPS patch provided.\n") unless $patch;
+die("Failed to detect an IPS patch.\n") unless $patch;
 
 my $rom = $ARGV[0];
-open(ROM, "+<$rom") or die "Can't open $rom";
+open(ROM, "+<$rom") or die "open() failed opening $rom\n";
 
 PATCH_LOOP: for (;;) {
-	read(PATCH, my $rom_offset, 3) or die "Read error";
+    my $rom_offset;
+	read(PATCH, $rom_offset, 3) or die("read() failed to read $rom_offset.\n");
 
 	last PATCH_LOOP if $rom_offset eq 'EOF';
 
@@ -48,28 +50,30 @@ PATCH_LOOP: for (;;) {
 	$rom_offset = hex( unpack("H*", $rom_offset) );
 
 	print "At address $rom_offset, " if $verbose;
-	seek(ROM, $rom_offset, SEEK_SET) or die "Failed seek";
+	seek(ROM, $rom_offset, SEEK_SET) or die("seek() failed to seek to $rom_offset.\n");
 
-	read(PATCH, my $data_size, 2) or die "Read error";
+	read(PATCH, my $data_size, 2) or die("read() failed reading data size.\n");
 	my $length = hex( unpack("H*", $data_size) );
 
 	if ($length) {
 		print "writing $length bytes of data.\n" if $verbose;
-		read(PATCH, my $data, $length) == $length or die "Read error";
+		read(PATCH, my $data, $length) == $length
+            or die("read() failed with $length bytes read.\n");
 		print ROM $data;
 	}
 	else { # RLE mode
-		read(PATCH, my $rle_size, 2) or die "Read error";
+		read(PATCH, my $rle_size, 2) or die("read() failed reading RLE size.\n");
 		$length = hex( unpack("H*", $rle_size) );
 
 		print "writing $length bytes of RLE data.\n" if $verbose;
-		read(PATCH, my $byte, 1) or die "Read error";
-		print ROM ($byte)x$length;
+		read(PATCH, my $byte, 1) == 1 or die "read() failed RLE data.\n";
+		print ROM "$byte" x $length;
 	}
 }
+
+print "Patched $rom with IPS file $patch.\n";
 
 close(PATCH);
 close(ROM);
 
-print "Patched $rom with $patch.\n";
 exit;
