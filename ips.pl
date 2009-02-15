af0d4a4dc15ba9fc1e45fe7ff82e3301b233e69f
chinesefood <not@ava.il.ab.le>
2009-02-14 23:10:31 -0600

    Added CHANGES, README, AND COPYING files for release.
    Revisions to copyright notices in POD and usage statements.

--- ips.pl
+++ ips.pl
@@ -98,12 +98,12 @@ ips.pl is a revision of a Perl script found in the wild (L<http://www.zophar.net
 
 Copyright 2003, 2009 chinesefood (eat.more.goawayspam.chinese.food@goawayspam.gmail.com)
 
-The original author is unknown at the time of this release.
-
-This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 2 of the License, or (at your option) any later version.
+This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 2 of the License, or (at your option) any later version.
 
 This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
 
-You should have received a copy of the GNU General Public License along with this program.  If not, see <http://www.gnu.org/licenses/>.
+You should have received a copy of the GNU General Public License along with this program; if not, write to the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+The original author is unknown at the time of release.
 
 =cut
